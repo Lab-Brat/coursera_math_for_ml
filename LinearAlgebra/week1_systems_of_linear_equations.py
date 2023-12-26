58719886c6eb9eed57f4d11a39c613cdd0867fd7
Lab-Brat <xoovoox@protonmail.com>
2023-12-26 22:04:23 +0300

    fix determinant calculation

--- LinearAlgebra/week1_systems_of_linear_equations.py
+++ LinearAlgebra/week1_systems_of_linear_equations.py
@@ -1,5 +1,25 @@
 from typing import List
 
+class Matrix:
+    def __init__(self, matrix: List) -> None:
+        self.matrix = matrix
+        self.determinant = self.calculate_determinant(self.matrix)
+
+    def calculate_determinant(self, matrix):
+        if len(matrix) == 2:
+            return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+
+        determinant = 0
+        for c in range(len(matrix)):
+            sub_matrix = [row[:c] + row[c+1:] for row in matrix[1:]]
+            
+            sign = (-1) ** c
+            sub_det = self.calculate_determinant(sub_matrix)
+            determinant += sign * matrix[0][c] * sub_det
+
+        return determinant
+
+
 class MatrixOps:
     def __init__(self, a: List[int], b: List[int], show = True) -> None:
         self.a = a
@@ -27,8 +47,8 @@ class MatrixOps:
         return new_matrix
 
     def find_type_using_sum(self) -> str:
-        a_sum = sum(a)
-        b_sum = sum(b)
+        a_sum = sum(self.a)
+        b_sum = sum(self.b)
         division = 0
         
         if a_sum >= b_sum:
@@ -73,11 +93,23 @@ class MatrixOps:
 
 
 if __name__ == "__main__":
-    a = [1, 1]
-    b = [2, 1]
-    m = MatrixOps(a, b)
-    m.addition()
-    m.multiplication()
-    m.find_type_using_sum()
-    m.find_type_using_determinant()
+    a = [
+        [5, 1],
+        [-1, 3],
+    ]
+    b = [
+        [2, -1],
+        [-6, 3],
+    ]
+    c = [
+        [1, 2, 3],
+        [4, 10, 6],
+        [7, 8, 9],
+    ]
+    ma = Matrix(a)
+    mb = Matrix(b)
+    mc = Matrix(c)
+    print(ma.determinant)
+    print(mb.determinant)
+    print(mc.determinant)
 
