f29dff37b952c0e7f753ee3787cc702ff03885fa
Lab-Brat <xoovoox@protonmail.com>
2024-01-06 20:28:09 +0300

    add size property for Matrix class

--- LinearAlgebra/matrix_from_scratch.py
+++ LinearAlgebra/matrix_from_scratch.py
@@ -1,19 +1,21 @@
-from typing import List
+from typing import List, Tuple
+
 
 class Matrix:
     def __init__(self, matrix: List) -> None:
         self.matrix = matrix
         self.determinant = self.calculate_determinant(self.matrix)
         self.matrix_type = self.get_matrix_type()
+        self.size = self.get_matrix_size()
 
     def calculate_determinant(self, matrix):
         if len(matrix) == 2:
-            return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
         determinant = 0
         for c in range(len(matrix)):
-            sub_matrix = [row[:c] + row[c+1:] for row in matrix[1:]]
-            
+            sub_matrix = [row[:c] + row[c + 1 :] for row in matrix[1:]]
+
             sign = (-1) ** c
             sub_det = self.calculate_determinant(sub_matrix)
             determinant += sign * matrix[0][c] * sub_det
@@ -26,9 +28,15 @@ class Matrix:
         else:
             return "non-singular"
 
+    def get_matrix_size(self) -> Tuple:
+        rows = len(self.matrix[0])
+        columns = len(self.matrix)
+
+        return (rows, columns)
+
 
 class MatrixOps:
-    def __init__(self, a: Matrix, b: Matrix, show = True) -> None:
+    def __init__(self, a: Matrix, b: Matrix, show=True) -> None:
         self.a = a.matrix
         self.b = b.matrix
         self.show = show
@@ -43,15 +51,8 @@ class MatrixOps:
 
         return Matrix(new_matrix)
 
-    def multiplication(self) -> Matrix:
-        new_matrix = []
-        for a, b in zip(self.a, self.b):
-            new_matrix.append(a + b)
-
-        if self.show:
-            print(new_matrix)
-
-        return Matrix(new_matrix)
+    def dot_product(self) -> float:
+        return 0.1
 
 
 if __name__ == "__main__":
@@ -62,4 +63,4 @@ if __name__ == "__main__":
     ]
     ma = Matrix(a)
     print(ma.determinant)
-    print(ma.matrix_type)
+    print(ma.size)
