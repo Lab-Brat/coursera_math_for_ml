905a8db69bd4e85b65eba097cb4a117418346be6
Lab-Brat <xoovoox@protonmail.com>
2024-01-06 21:43:57 +0300

    add vector ops

--- LinearAlgebra/matrix_from_scratch.py
+++ LinearAlgebra/matrix_from_scratch.py
@@ -4,9 +4,14 @@ from typing import List, Tuple
 class Matrix:
     def __init__(self, matrix: List) -> None:
         self.matrix = matrix
-        self.determinant = self.calculate_determinant(self.matrix)
-        self.matrix_type = self.get_matrix_type()
-        self.size = self.get_matrix_size()
+        self.shape = self.get_matrix_shape()
+
+        if self.shape[1] > 1:
+            self.determinant = self.calculate_determinant(self.matrix)
+            self.matrix_type = self.get_matrix_type()
+        else:
+            self.determinant = None
+            self.matrix_type = "vector"
 
     def calculate_determinant(self, matrix):
         if len(matrix) == 2:
@@ -28,22 +33,22 @@ class Matrix:
         else:
             return "non-singular"
 
-    def get_matrix_size(self) -> Tuple:
-        rows = len(self.matrix[0])
-        columns = len(self.matrix)
+    def get_matrix_shape(self) -> Tuple:
+        rows = len(self.matrix)
+        columns = len(self.matrix[0])
 
         return (rows, columns)
 
 
 class MatrixOps:
     def __init__(self, a: Matrix, b: Matrix, show=True) -> None:
-        self.a = a.matrix
-        self.b = b.matrix
+        self.a = a
+        self.b = b
         self.show = show
 
     def addition(self) -> Matrix:
         new_matrix = []
-        for a, b in zip(self.a, self.b):
+        for a, b in zip(self.a.matrix, self.b.matrix):
             new_matrix.append(a + b)
 
         if self.show:
@@ -51,16 +56,34 @@ class MatrixOps:
 
         return Matrix(new_matrix)
 
-    def dot_product(self) -> float:
-        return 0.1
+    def dot_product(self) -> float | None:
+        if self.a.matrix_type == "vector" and self.b.matrix_type == "vector":
+            product = 0.0
+            for a, b in zip(self.a.matrix, self.b.matrix):
+                product += a[0] * b[0]
+
+            if self.show:
+                print(product)
+            return product
+        else:
+            print("can only calculate dor product for vectors")
 
 
 if __name__ == "__main__":
-    a = [
-        [-3, 8, 1],
-        [2, 2, -1],
-        [-5, 6, 2],
+    # a = [
+    #     [-3, 8, 1],
+    #     [2, 2, -1],
+    #     [-5, 6, 2],
+    # ]
+    # ma = Matrix(a)
+    a1 = [
+        [2],
+        [-3],
     ]
-    ma = Matrix(a)
-    print(ma.determinant)
-    print(ma.size)
+    b1 = [
+        [1],
+        [6],
+    ]
+    A1 = Matrix(a1)
+    B1 = Matrix(b1)
+    MatrixOps(A1, B1, show=True).dot_product()
