ef3ae258d3a07ab73b64ebb2152725af3dd482a1
Lab-Brat <xoovoox@protonmail.com>
2024-01-28 22:30:01 +0300

    add gradient descent from scratch

--- Calculus/gradient_descent.py
+++ Calculus/gradient_descent.py
@@ -51,8 +51,43 @@ class LinReg:
         b = lr_sci.intercept_
         return m[0][0], b[0]  # noqa
 
+    def _normalize(self, X, Y):
+        X = (X - np.mean(X)) / np.std(X)
+        Y = (Y - np.mean(Y)) / np.std(Y)
+        return X, Y
+
+    def _denormalize(self, X, Y):
+        pass
+
+    def _sum_of_squares_cost_function(self, m, b, X, Y):
+        return 1 / (2 * len(Y)) * np.sum((m * X + b - Y) ** 2)
+
+    def _sum_of_squares_partial_divs(self, m, b, X, Y):
+        dFdm = 1 / len(X) * np.dot(m * X + b - Y, X)
+        dFdb = 1 / len(X) * np.sum(m * X + b - Y)
+        return dFdm, dFdb
+
+    def gradient_descent(
+        self, m, b, X, Y, learning_rate=0.001, num_iterations=1000, show_cost=False
+    ):
+        for _ in range(num_iterations):
+            dFdm, dFdb = self._sum_of_squares_partial_divs(m, b, X, Y)
+            m = m - learning_rate * dFdm
+            b = b - learning_rate * dFdb
+
+            if show_cost:
+                print(f"cost: {self._sum_of_squares_cost_function(m, b, X, Y)}")
+        return m, b
+
     def linear_regression_from_scratch(self, X, Y):
-        return (0, 0)
+        X, Y = self._normalize(X, Y)
+        m, b = 0, 0
+        learning_rate = 1.2
+        iterations = 30
+        m, b = self.gradient_descent(
+            m, b, X, Y, learning_rate, iterations, show_cost=True
+        )
+        return m, b
 
     def calculate(self) -> None | Tuple:
         x = self.data[self.data.columns[0]]
@@ -80,5 +115,5 @@ class LinReg:
 if __name__ == "__main__":
     data_file = "./data/tvmarketing.csv"
     dataset = DataSet(data_file, show=True)
-    method = "sci"
+    method = "scr"
     LinReg(dataset, show=True, method=method).calculate()
