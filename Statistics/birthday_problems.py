d69375e25bc2e02c4f752c8c47527a498ab0ca5b
Lab-Brat <xoovoox@protonmail.com>
2024-02-14 21:53:52 +0300

    add analytical solution for problem 1

--- Statistics/birthday_problems.py
+++ Statistics/birthday_problems.py
@@ -9,15 +9,19 @@ def simulate(pfunc, student_num, runs):
 # what is the value of n such that the probability of having 
 # a match is greater than or equal to 0.5?
 
-def problem_1(student_num):
+def problem_1_simulate(student_num):
     birthday = random.randint(1, 365)
     birthdays = [random.randint(1, 365) for _ in range(student_num)]
     return birthday in birthdays
 
+def problem_1_analytic(student_num):
+    prob = 1 - (1 - 1/365) ** student_num
+    return f"{prob:.2f}"
 
-students = 255
+students = 365
 runs = 100
 sums = 0
 for sim in range(100):
-    sums += simulate(problem_1, students, runs)
-print(sums / 100)
+    sums += simulate(problem_1_simulate, students, runs)
+print(f"{sums / 100:.2f}")
+print(problem_1_analytic(students))
